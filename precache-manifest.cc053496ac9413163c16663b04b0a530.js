ed313d972dae40b2739229c2fc1dbe61025cc9b4
spocot <spencerkocot@gmail.com>
2023-01-18 01:44:48 -0500

    Updates

---
+++ precache-manifest.cc053496ac9413163c16663b04b0a530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8817e84d70a9ce3e66b3",
+    "url": "/react-gh-pages/static/css/main.9c68755f.chunk.css"
+  },
+  {
+    "revision": "8817e84d70a9ce3e66b3",
+    "url": "/react-gh-pages/static/js/main.8817e84d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-gh-pages/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d141efd1a4bd921c58fbdd6b4d1f91fd",
+    "url": "/react-gh-pages/index.html"
+  }
+];
